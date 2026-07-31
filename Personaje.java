8c295c69b88c6babb50e04480e2cdd4727164e51
Luis Miguel Jaramillo Peña <lmjaramillo_1112@unihumboldt.edu.co>
2026-07-31 16:38:42 -0500

    Add files via upload
    Nivel 7 hecho.

---
+++ Personaje.java
@@ -0,0 +1,13 @@
+public class Personaje {
+    public String nombre;
+    public String clase;
+    public int nivel;
+    public int vida;
+    public int vidaMaxima;
+    public int ataque;
+    public int defensa;
+    public double oro;
+    public boolean estaVivo;
+    public String arma;
+    public boolean esVenenoso;
+}
